a5de7c0a11c5fee8ca61a2a1b002042ca28137bc
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-02-24 12:29:00 -0800

    Add files via upload

---
+++ ItemDB.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+
+    public List<Item> itemDB = new List<Item>();
+
+    void Start()
+    {
+       itemDB[0].Action();
+    }
+}
